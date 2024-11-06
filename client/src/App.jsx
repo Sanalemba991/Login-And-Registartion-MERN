53a967f3adf4d6ef5717a6741cce3dec4db077a7
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-06 20:52:42 +0530

    SignUp complete

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import SignUp from "./SignUp";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LogIn from "./LogIn";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-          <Route path='/register' element={SignUp}></Route>
-          <Route path='/login' element={LogIn}></Route>
+          <Route path='/register' element={<SignUp/>}></Route>
+          <Route path='/login' element={<LogIn/>}></Route>
         </Routes>
       </BrowserRouter>
     </>
--- client/src/SignUp.jsx
+++ client/src/SignUp.jsx
@@ -1,34 +1,57 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 
 function SignUp() {
+    const [name,setName]=useState("")
+    const [email,setEmail]=useState('')
+    const [password,setPassword]=useState('')
+    
+    const suBmit=(e)=>{
+
+        e.preventDefault()
+
+        axios.post('',{name,email,password})
+        .then(result =>console.log(result))
+        .catch(err =>console.log(err))
+
+    }
   return (
+
     <div>
       <h2>Register</h2>
-      <form>
+      <form onSubmit={submit}>
         <div>
           <label htmlFor="name">
             <strong>Name</strong>
           </label>
-          <input type="text" placeholder="Name" name="name" />
+          <input type="text"
+           placeholder="Name"
+           name="name"
+          
+          onChange={(e)=>setName(e.target.value)}/>
         </div>
         <div>
           <label htmlFor="email">
             <strong>Email</strong>
           </label>
-          <input type="email" placeholder="Email" name="email" />
+          <input type="email" placeholder="Email" name="email"
+            onChange={(e)=>setEmail(e.target.value)}/> 
         </div>
         <div>
           <label htmlFor="password">
             <strong>Password</strong>
           </label>
-          <input type="password" placeholder="Password" name="password" />
+          <input type="password" placeholder="Password" name="password" 
+            onChange={(e)=>setPassword(e.target.value)}/>
+          
         </div>
-        <div>
+        
           <button type="submit">Register</button>
+          </form>
           <p>Already have account</p>
-          <button>Login</button>
-        </div>
-      </form>
+          <Link to="/login">Login</Link>
+        
+ 
     </div>
   );
 }
