2d8412bac2d9d9672eaf35a0530b6417d7c9a064
Sanalemba991 <laitonjamsanalembameitei99@gmail.com>
2024-11-06 20:15:57 +0530

    sign up lautout given

--- client/src/SignUp.jsx
+++ client/src/SignUp.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { useState } from "react";
 
 function SignUp() {
   return (
     <div>
-      Signup
+      <h2>Register</h2>
+      <form>
+        <div>
+          <label htmlFor="name">
+            <strong>Name</strong>
+          </label>
+          <input type="text" placeholder="Name" name="name" />
+        </div>
+        <div>
+          <label htmlFor="email">
+            <strong>Email</strong>
+          </label>
+          <input type="email" placeholder="Email" name="email" />
+        </div>
+        <div>
+          <label htmlFor="password">
+            <strong>Password</strong>
+          </label>
+          <input type="password" placeholder="Password" name="password" />
+        </div>
+        <div>
+          <button type="submit">Sign Up</button>
+          <p>Already have account</p>
+          <button>Login</button>
+        </div>
+      </form>
     </div>
-  )
+  );
 }
 
-export default SignUp
+export default SignUp;
